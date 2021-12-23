41fea95045d3e5924de0884c13f914b25c8212c0
Ritesh Sadhu Salian <rsalian95@gmail.com>
2021-12-23 12:54:19 +0530

    test commit for both user and user-ws

--- user-api/src/main/java/in/home/user/api/model/Role.java
+++ user-api/src/main/java/in/home/user/api/model/Role.java
@@ -1,22 +1,22 @@
 package in.home.user.api.model;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Role {
-  private Long id;
-  private String roleName;
-  private boolean active;
-  private LocalDateTime createdOn;
-  private Long createdBy;
-  private LocalDateTime updatedOn;
-  private Long updatedBy;
+
+    private Long id;
+    private String roleName;
+    private boolean active;
+    private LocalDateTime createdOn;
+    private Long createdBy;
+    private LocalDateTime updatedOn;
+    private Long updatedBy;
 }
